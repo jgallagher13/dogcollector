8d46253f40228bfd654f7619a90c8fa486d13ccd
jgallagher13 <jenngallagher13@gmail.com>
2023-09-28 09:59:20 -0600

    add details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
-dogs = [
-  {'name': 'Socks', 'breed': 'German Shepherd/ Greyhound', 'description': 'long and lanky', 'age': 3},
-  {'name': 'Obi-wan', 'breed': 'Cattle Dog', 'description': 'wild and snuggly', 'age': 2},
-]
+from .models import Dog
 
 
 # Create your views here.
@@ -14,6 +11,13 @@ def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', {
     'dogs': dogs
+  })
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 
+    'dog': dog 
   })
