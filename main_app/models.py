481e0dc3c1f6929c15dd18ae2baad570422e2b0d
jgallagher13 <jenngallagher13@gmail.com>
2023-09-29 08:53:49 -0600

    add crud

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
@@ -9,3 +9,6 @@ class Dog(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 
@@ -21,3 +21,15 @@ def dogs_detail(request, dog_id):
   return render(request, 'dogs/detail.html', { 
     'dog': dog 
   })
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = '__all__'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs'
